0b10a6ba0fee2e3a93d6d005ee60f7b926a24ded
Ademola <adigun673@gmail.com>
2019-06-11 12:39:59 +0200

    Created menu class

---
+++ GuessWhat/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessWhat
+{
+    class Menu
+    {
+        void handleJoinLobby(object sender, EventArgs e)
+        {
+
+        }
+
+        void handleCreateLobby(object sender, EventArgs e)
+        {
+
+        }
+
+        void handleOpenSettings(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
