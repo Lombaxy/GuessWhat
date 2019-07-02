8ed7fabb69a1d8da32f6d904d502c31fdad59b37
Robert Jeffrey <rob.jeffrey@student.stenden.com>
2019-07-02 23:06:03 +0200

    canvas class frame

---
+++ GuessWhat/Canvas.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using Windows.UI;
+
+namespace GuessWhat
+{
+    class Canvas
+    {
+        //private ArrayList<Tool> Tools = new ArrayList();
+
+
+        //Canvas()
+        //{
+
+        //}
+
+        //public Tool GetCurrentTool()
+        //{
+
+        //}
+
+        //public void SetCanvasColor(Color color)
+        //{
+
+        //}
+
+        //public Color GetCanvasColor()
+        //{
+
+        //}
+
+        //public void SetDrawingColor()
+        //{
+
+        //}
+
+        //public Color GetDrawingColor()
+        //{
+
+        //}
+
+        //public int SetToolWidth(int width)
+        //{
+
+        //}
+
+        //public int GetToolWidth()
+        //{
+
+        //}
+
+        //public ArrayList<Tool> GetTools()
+        //{
+
+        //}
+
+        //public Tool GetCurrentTool()
+        //{
+
+        //}
+
+        //public void ChooseTool(Tool tool)
+        //{
+
+        //}
+
+        //public void UpdateCanvas()
+        //{
+
+        //}
+
+        //public void onStroke()
+        //{
+
+        //}
+
+
+    }
+
+
+}
